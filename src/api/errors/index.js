995fa8cc676094a53b9925f71dc285db07167b69
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-06-20 00:21:59 +0700

    Perapihan kode via ESLINT

--- src/api/errors/index.js
+++ src/api/errors/index.js
@@ -1,10 +1,10 @@
 const ErrorHandler = require('./handler');
 
 module.exports = {
-    name: 'Error Handler for openMusic API',
-    version: '1.0.0',
-    register: (server) => {
-      const errorHandler = new ErrorHandler();
-      server.ext('onPreResponse', errorHandler.errorHandler);
-    },
-  };
+  name: 'Error Handler for openMusic API',
+  version: '1.0.0',
+  register: (server) => {
+    const errorHandler = new ErrorHandler();
+    server.ext('onPreResponse', errorHandler.errorHandler);
+  },
+};
